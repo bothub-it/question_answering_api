5b07431d09aa03b2277f5d26519549b3e4b0740d
Lucas Agra <lucasomena@hotmail.com>
2021-08-25 11:47:08 -0300

    Refactoring utils.py

--- utils.py
+++ utils.py
@@ -1,60 +1,34 @@
 import settings
 
+config_args = {
+    "n_best_size": 3,
+    "num_train_epochs": 2,
+    "max_seq_length": settings.MAX_SEQ_LENGTH,
+    "doc_stride": int(settings.MAX_SEQ_LENGTH*0.8),
+    "train_batch_size": 16,
+    "gradient_accumulation_steps": 1,
+    "eval_batch_size": 16,
+    "save_steps": -1,
+    "save_model_every_epoch": True,
+    "evaluate_during_training_steps": -1,
+    "evaluate_during_training": True,
+    "evaluate_during_training_verbose": True,
+    "use_cached_eval_features": True
+}
+
 model_info = {
     'pt_br': {
-        'args': {
-            "n_best_size": 3,
-            "num_train_epochs": 2,
-            "max_seq_length": settings.MAX_SEQ_LENGTH,
-            "doc_stride": int(settings.MAX_SEQ_LENGTH*0.8),
-            "train_batch_size": 16,
-            "gradient_accumulation_steps": 1,
-            "eval_batch_size": 16,
-            "save_steps": -1,
-            "save_model_every_epoch": True,
-            "evaluate_during_training_steps": -1,
-            "evaluate_during_training": True,
-            "evaluate_during_training_verbose": True,
-            "use_cached_eval_features": True
-        },
+        'args': config_args,
         'type': 'bert',
         'dir': 'pt_br'
     },
     'en': {
-        'args': {
-            "n_best_size": 3,
-            "num_train_epochs": 2,
-            "max_seq_length": settings.MAX_SEQ_LENGTH,
-            "doc_stride": int(settings.MAX_SEQ_LENGTH*0.8),
-            "train_batch_size": 16,
-            "gradient_accumulation_steps": 1,
-            "eval_batch_size": 16,
-            "save_steps": -1,
-            "save_model_every_epoch": True,
-            "evaluate_during_training_steps": -1,
-            "evaluate_during_training": True,
-            "evaluate_during_training_verbose": True,
-            "use_cached_eval_features": True
-        },
+        'args': config_args,
         'type': 'roberta',
         'dir': 'en'
     },
     'multilang': {
-        'args': {
-            "n_best_size": 3,
-            "num_train_epochs": 2,
-            "max_seq_length": settings.MAX_SEQ_LENGTH,
-            "doc_stride": int(settings.MAX_SEQ_LENGTH*0.8),
-            "train_batch_size": 16,
-            "gradient_accumulation_steps": 1,
-            "eval_batch_size": 16,
-            "save_steps": -1,
-            "save_model_every_epoch": True,
-            "evaluate_during_training_steps": -1,
-            "evaluate_during_training": True,
-            "evaluate_during_training_verbose": True,
-            "use_cached_eval_features": True
-        },
+        'args': config_args,
         'type': 'bert',
         'dir': 'multilang'
     },
